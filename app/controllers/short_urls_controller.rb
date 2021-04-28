f332d4ac1904af5f80bb881056b7e98a17f2d62b
John Verdone <jmverdone@gmail.com>
2021-04-27 23:46:17 -0400

    changed method to shorten_code

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -11,12 +11,13 @@ class ShortUrlsController < ApplicationController
   end
 
   def create
+    
     short_url = ShortUrl.new
     short_url.full_url = params[:full_url]
     
     #only save on valid urls
-
     if short_url.save
+      short_url.shorten_code
       render json: short_url
     else
       render json: short_url.errors
@@ -25,17 +26,14 @@ class ShortUrlsController < ApplicationController
   end
 
   def show
-   url = ShortUrl.short_code(params[:id])
-
+    url = ShortUrl.find_by(short_code: params[:id])
+    
     if url
       url.update(click_count: url.click_count += 1) 
       redirect_to url.full_url
     else
       render :json => @error_object.to_json, :status => 404
-
     end
-      
-
   end
 
 end
