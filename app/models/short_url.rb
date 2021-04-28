be76b7298fc154a7f25c636f662211dc43dd0050
John Verdone <jmverdone@gmail.com>
2021-04-27 21:52:16 -0400

    generate short code based on id

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,10 +1,20 @@
 class ShortUrl < ApplicationRecord
+  require 'open-uri'
 
   CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
 
   validate :validate_full_url
 
-  def short_code
+  def self.short_code(short_code)  
+    id = 0
+    digit = 0
+    value.reverse.each_char do |char|
+      num = CHARACTERS.index(char)
+      id += num * (CHARACTERS.count ** digit)
+      digit += 1
+    end
+    
+    ShorUrl.find(id)
   end
 
   def update_title!
@@ -14,7 +24,6 @@ class ShortUrl < ApplicationRecord
 
   def validate_full_url
     #validate any urls with an "OK" response and 200 status
-
     begin
       raise errors.add(:errors, "Full url is not a valid url") unless open(full_url).status == ["200","OK"]
     rescue
