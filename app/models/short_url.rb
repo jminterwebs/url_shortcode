8a784c4997e85399dfca40ddef8bd953ceb91474
John Verdone <jmverdone@gmail.com>
2021-04-27 21:01:18 -0400

    validates url

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -13,6 +13,15 @@ class ShortUrl < ApplicationRecord
   private
 
   def validate_full_url
+    #validate any urls with an "OK" response and 200 status
+
+    begin
+      raise errors.add(:errors, "Full url is not a valid url") unless open(full_url).status == ["200","OK"]
+    rescue
+      errors.add(:errors, "Full url is not a valid url")
+    end
+
+
   end
 
 end
