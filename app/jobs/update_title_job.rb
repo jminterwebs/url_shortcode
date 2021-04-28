c7eb73c576b3b9549b2cf1094e71728623586c55
John Verdone <jmverdone@gmail.com>
2021-04-27 23:46:28 -0400

    pass in object

--- app/jobs/update_title_job.rb
+++ app/jobs/update_title_job.rb
@@ -1,7 +1,9 @@
 class UpdateTitleJob < ApplicationJob
   queue_as :default
 
-  def perform(short_url_id)
-
+  def perform(short_url)
+    doc = Nokogiri::HTML(open("#{short_url.full_url}"))
+    title = doc.css("title").text
+    short_url.update(title: title)
   end
 end
